31e6aa282a6a9a1d1c77f0df2585eb56bd35fbf3
Rataprens <Rataprens@gmail.com>
2025-02-04 13:02:41 -0300

    chore: update index to add more meta tags in Helmet for improved SEO

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,9 +19,27 @@ const IndexPage: React.FC = () => {
   return (
     <>
       <Helmet>
+        {/* Meta Básicos */}
+        <html lang="en" /> 
+        <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#121212" />
+        <meta name="keywords" content="CWIF, burning stats, token metrics, blockchain stats" />
         <title>Catwifhat | CWIF FORGE 🔥⚒️</title>
-        {/* Puedes agregar más etiquetas meta o títulos aquí */}
+        <meta name="description" content="Track CWIF burning statistics and insights in real-time." />
+        
+        {/* Open Graph (Para compartir en redes sociales) */}
+        <meta property="og:title" content="Catwifhat | CWIF FORGE 🔥⚒️" />
+        <meta property="og:description" content="Track CWIF burning statistics and insights in real-time." />
+        <meta property="og:image" content="https://cwifforge.netlify.app/static/CWIF-230bb48074e9c04cd8aea1ae2a8a208d.png" />
+        <meta property="og:url" content="https://cwifforge.netlify.app/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Cards (Para compartir en Twitter) */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Catwifhat | CWIF FORGE 🔥⚒️" />
+        <meta name="twitter:description" content="Track CWIF burning statistics and insights in real-time." />
+        <meta name="twitter:image" content="https://cwifforge.netlify.app/static/CWIF-230bb48074e9c04cd8aea1ae2a8a208d.png" />
       </Helmet>
       <GlobalStyle />
       <Layout>
